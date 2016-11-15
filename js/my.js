78bacbf8c1b823153053beda9a140e0d10c9ebf5
Yuancheng Zhang <old.far.hot@gmail.com>
2016-11-15 13:33:36 -0800

    Update 3 blogs

--- js/my.js
+++ js/my.js
@@ -1,26 +1,37 @@
 $(document).ready(function(){
-	var f = function() {
-		$(".article-title").click(function(){
-			$("#includedContent").load("/blog/blog-20150611.html", g);
-		});
-	};
+
+    var e = function () {
+        var blogs = document.getElementsByClassName("article-title");
+        for (var i = 0; i < blogs.length; i++) {
+            var blog = blogs[i];
+            blog.getElementsByTagName("d")[0].innerHTML = blog.getAttribute("id");
+        }
+    }
+
+    var f = function() {
+        $(".article-title").click(function(){
+            var blogId = (event.target).getAttribute("id");
+            $("#includedContent").load("blog/blog-" + blogId + ".html", g);
+        });
+        e();
+    };
 
 	var g = function() {
 		$(".back-to-blog-list").click(function(){
-			$("#includedContent").load("/blog/blog-list.html", f);
+			$("#includedContent").load("blog/blog-list.html", f);
 		});
 	};
 
 	$(function(){
-		$("#includedHeader").load("/html/header.html"); 
+		$("#includedHeader").load("html/header.html");
 	});
 
 	$(function(){
-		$("#includedFooter").load("/html/footer.html"); 
+		$("#includedFooter").load("html/footer.html");
 	});
 
 	$(function(){
-		$("#includedContent").load("/blog/blog-list.html", f);
+		$("#includedContent").load("blog/blog-list.html", f);
 	});
 });
 
